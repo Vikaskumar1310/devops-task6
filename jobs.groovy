0c22299df4bea5f82253f224c79037ec81aafd5e
vikas kumar <vkk1310@gmail.com>
2020-07-15 15:13:06 +0530

    groovy script added

---
+++ jobs.groovy
@@ -0,0 +1,57 @@
+job("task6-job1")
+{
+	description("this Job will clone repo from GitHub and copy code to my os")
+	scm {
+		github("Vikaskumar1310/devops-task3","master")
+	}
+	triggers {
+		scm("* * * * *")
+	}
+	steps{
+		shell(""
+		sudo cp -v * /myjaky_OS/root/task6
+		sudo rm -f * 
+                "")
+	}
+}
+
+job("task6-job2")
+{
+       description("this job launch the webserver")
+       trigger{
+              upstream("task6-job1","SUCCESS")
+        }
+        steps{
+               shell("sh /myjaky_OS/root/task6/job2.sh")
+        }
+}
+
+job("task6-job3")
+{
+       description("this job test the webpage according to the lang and send the mail to developer if any code fails")
+       trigger{
+              upstream("task6-job2","SUCCESS")
+        }
+        steps{
+               shell("sh /myjaky_OS/root/task6/job3.sh")
+        }
+        publishers{
+                   mailer('vkk1310@gmail.com',false,false)
+        }
+}
+buildPipelineView("devops-task6"){
+        fileterBuildQueue()
+        filterExecutors()
+        title("task6-build-pipeline")
+        displayedBuilds(3)
+        selectedJob("task6-job1")
+        alwaysAllowManualTrigger()
+        showPipelineParameters()
+        refreshFrequency(30)
+}
+
+
+
+
+
+
